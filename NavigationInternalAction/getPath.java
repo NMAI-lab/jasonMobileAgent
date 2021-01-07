eedf44c74aa1b489668e1d840591cc50fc222839
Patrick Gavigan <pgavigan@gmail.com>
2021-01-07 13:41:18 -0500

    Adding internal action version of getPath

---
+++ NavigationInternalAction/getPath.java
@@ -0,0 +1,23 @@
+// Internal action code for project search.mas2j
+
+package NavigationInternalAction;
+
+import jason.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+
+public class getPath extends DefaultInternalAction {
+
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        // execute the internal action
+        ts.getAg().getLogger().info("executing internal action 'NavigationInternalAction.getPath'");
+        if (true) { // just to show how to throw another kind of exception
+            throw new JasonException("not implemented!");
+        }
+
+        // everything ok, so returns true
+        return true;
+    }
+}
+
